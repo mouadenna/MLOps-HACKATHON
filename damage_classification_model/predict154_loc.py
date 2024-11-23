00687d13bcb35e5b39192f2d7bc255374d140a3a
Mouad En-nasiry <90518486+mouadenna@users.noreply.github.com>
2024-11-23 19:12:12 +0100

    Update predict154_loc.py

--- damage_classification_model/predict154_loc.py
+++ damage_classification_model/predict154_loc.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
         model = SeNet154_Unet_Loc(pretrained=None)
         model = nn.DataParallel(model)
         print("=> loading checkpoint '{}'".format(snap_to_load))
-        checkpoint = torch.load(path.join(models_folder, snap_to_load), map_location=device)
+        checkpoint = torch.load(path.join(models_folder, snap_to_load), map_location="cuda")
         loaded_dict = checkpoint['state_dict']
         sd = model.state_dict()
         for k in model.state_dict():
